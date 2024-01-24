3a7c63f0812cc187fa42acd43d6264c9f97fc249
oott123 <git@public.oott123.com>
2024-01-24 23:44:06 +0800

    feat: migrate to yarn and code-style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-Hans">
       <body>{children}</body>
     </html>
-  );
+  )
 }
