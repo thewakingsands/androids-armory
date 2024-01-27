ddf0927d3698bf5457827c4b4c1983cf1cc209a4
oott123 <git@public.oott123.com>
2024-01-27 10:57:19 +0800

    feat: real menu

--- components/aa-navbar-item-text-link.tsx
+++ components/aa-navbar-item-text-link.tsx
@@ -1,21 +1,53 @@
 'use client'
 
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/navbar'
 import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@nextui-org/dropdown'
-import { Button } from '@nextui-org/button'
 import { Link } from '@nextui-org/link'
-import { useCallback, useRef, useState } from 'react'
+import { NavbarItem } from '@nextui-org/navbar'
+import React, { forwardRef, useRef, useState } from 'react'
 import useEvent from 'react-use-event-hook'
-import {
-  AppPaymentSymbol,
-  ArmouryBody,
-  ArmouryMainArm,
-  ArmouryRing,
-  ClassJob008,
-  ItemCategoryCrp,
-} from '../app/_components/_app-icons'
 
-export function AaNavbarItemTextLink({ title, icon, href }: { title: string; icon: React.ReactNode; href: string }) {
+const NavItemBody = forwardRef<
+  HTMLDivElement,
+  {
+    handleOpen: () => void
+    handleClose: () => void
+    href: string
+    icon: React.ReactNode
+    title: string
+    portalContainer: React.RefObject<HTMLDivElement>
+  }
+>(function ({ handleClose, handleOpen, href, icon, title, portalContainer }, ref) {
+  return (
+    <div
+      ref={ref}
+      className="relative before:inline-block before:absolute before:w-full before:h-2 before:bg-primary-500 before:-top-4 hover:before:translate-y-1 before:transition-transform hover:text-primary-500 transition-colors"
+      onMouseEnter={handleOpen}
+      onMouseLeave={handleClose}
+    >
+      <Link href={href} className="w-20 h-full relative flex justify-center items-center text-inherit">
+        <div className="flex flex-col gap-2 justify-center items-center">
+          <div className="text-2xl inline-block" style={{ width: '1em', height: '1em' }}>
+            {icon}
+          </div>
+          <p className="text-sm">{title}</p>
+        </div>
+      </Link>
+      <div className="text-foreground-900" ref={portalContainer}></div>
+    </div>
+  )
+})
+
+export function AaNavbarItemTextLink({
+  title,
+  icon,
+  href,
+  menu,
+}: {
+  title: string
+  icon: React.ReactNode
+  href: string
+  menu?: Array<{ key: string; icon: React.ReactNode; title: React.ReactNode }>
+}) {
   const debuggingMenu = false
 
   const [open, setOpen] = useState(false)
@@ -36,25 +68,11 @@ export function AaNavbarItemTextLink({ title, icon, href }: { title: string; ico
     }, 100) as any as number
   })
 
-  return (
+  return menu ? (
     <Dropdown isOpen={open} triggerScaleOnOpen={false} portalContainer={portalContainer.current ?? undefined}>
       <NavbarItem className="h-full">
         <DropdownTrigger>
-          <div
-            className="relative before:inline-block before:absolute before:w-full before:h-2 before:bg-primary-500 before:-top-4 hover:before:translate-y-1 before:transition-transform hover:text-primary-500 transition-colors"
-            onMouseEnter={handleOpen}
-            onMouseLeave={handleClose}
-          >
-            <Link href={href} className="w-20 h-full relative flex justify-center items-center text-inherit">
-              <div className="flex flex-col gap-2 justify-center items-center">
-                <div className="text-2xl inline-block" style={{ width: '1em', height: '1em' }}>
-                  {icon}
-                </div>
-                <p className="text-sm">{title}</p>
-              </div>
-            </Link>
-            <div className="text-foreground-900" ref={portalContainer}></div>
-          </div>
+          <NavItemBody {...{ handleOpen, handleClose, href, icon, title, portalContainer }} />
         </DropdownTrigger>
       </NavbarItem>
       <DropdownMenu
@@ -63,22 +81,16 @@ export function AaNavbarItemTextLink({ title, icon, href }: { title: string; ico
         onMouseLeave={handleClose}
         // itemClasses={{ base: 'py-2' }}
       >
-        <DropdownItem key="main-arm" startContent={<ArmouryMainArm className="w-6" />}>
-          武器
-        </DropdownItem>
-        <DropdownItem key="body" startContent={<ArmouryBody className="w-6" />}>
-          防具
-        </DropdownItem>
-        <DropdownItem key="fashion" startContent={<AppPaymentSymbol className="w-6" />}>
-          时装
-        </DropdownItem>
-        <DropdownItem key="accessories" startContent={<ArmouryRing className="w-6" />}>
-          饰品
-        </DropdownItem>
-        <DropdownItem key="tool" startContent={<ItemCategoryCrp className="w-6" />}>
-          工具
-        </DropdownItem>
+        {menu.map((menuItem) => (
+          <DropdownItem key={menuItem.key} startContent={menuItem.icon}>
+            {menuItem.title}
+          </DropdownItem>
+        ))}
       </DropdownMenu>
     </Dropdown>
+  ) : (
+    <NavbarItem className="h-full">
+      <NavItemBody {...{ handleOpen, handleClose, href, icon, title, portalContainer }} />
+    </NavbarItem>
   )
 }
--- components/aa-root-layout.tsx
+++ components/aa-root-layout.tsx
@@ -2,11 +2,29 @@ import { Link } from '@nextui-org/link'
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/navbar'
 import React from 'react'
 import { FaUmbrella } from 'react-icons/fa'
-import { AppHomeMenuInventory, FellowActivity18, ItemCategoryDye, Market } from '../app/_components/_app-icons'
+import {
+  AppHomeMenuInventory,
+  AppPaymentSymbol,
+  ArmouryBody,
+  ArmouryMainArm,
+  ArmouryRing,
+  FellowActivity18,
+  ItemCategoryCrp,
+  ItemCategoryDye,
+  Market,
+} from '../app/_components/_app-icons'
 import { AALogo } from './aa-logo'
 import { AaNavbarItemTextLink } from './aa-navbar-item-text-link'
 
 export function AaRootLayout({ children }: { children: React.ReactNode }) {
+  const armouryCategoryMenu = [
+    { key: 'arm', icon: <ArmouryMainArm className="w-6" />, title: '武器' },
+    { key: 'body', icon: <ArmouryBody className="w-6" />, title: '防具' },
+    { key: 'fashion', icon: <AppPaymentSymbol className="w-6" />, title: '时装' },
+    { key: 'accessories', icon: <ArmouryRing className="w-6" />, title: '饰品' },
+    { key: 'tool', icon: <ItemCategoryCrp className="w-6" />, title: '工具' },
+  ]
+
   return (
     <div className="aa-root-layout">
       <Navbar className="py-2">
@@ -14,8 +32,8 @@ export function AaRootLayout({ children }: { children: React.ReactNode }) {
           <AALogo />
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex flex-1" justify="center">
-          <AaNavbarItemTextLink title="系列" icon={<FellowActivity18 />} href="/" />
-          <AaNavbarItemTextLink title="单品" icon={<AppHomeMenuInventory />} href="/" />
+          <AaNavbarItemTextLink title="系列" icon={<FellowActivity18 />} href="/" menu={armouryCategoryMenu} />
+          <AaNavbarItemTextLink title="单品" icon={<AppHomeMenuInventory />} href="/" menu={armouryCategoryMenu} />
           <AaNavbarItemTextLink title="时尚配饰" icon={<FaUmbrella />} href="/" />
           <AaNavbarItemTextLink title="染剂" icon={<ItemCategoryDye />} href="/" />
           <AaNavbarItemTextLink title="专题" icon={<Market />} href="/" />
