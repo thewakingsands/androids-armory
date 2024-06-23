933e902a003139ed8ff0b928050ba633e6955d21
oott123 <git@public.oott123.com>
2024-06-23 19:49:43 +0800

    feat: home page category

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+import Link from 'next/link'
 import { HomePageH1 } from './_components/HomePageH1'
 import { HomePageMainArm } from './_components/HomePageMainArm'
 import { HomePageTilesRow } from './_components/HomePageTilesRow'
-import { armouryCategory, armouryCategoryRecord } from './_constants/armouryCategory'
+import {
+  ArmouryBody,
+  ArmouryBracelets,
+  ArmouryEarrings,
+  ArmouryFeet,
+  ArmouryHands,
+  ArmouryHead,
+  ArmouryLegs,
+  ArmouryMainArm,
+  ArmouryNecklace,
+  ArmouryRing,
+  ItemCategoryDye,
+  ItemCategorySoulCrystal,
+} from './_components/_app-icons'
+import { armouryCategory } from './_constants/armouryCategory'
+
+const homePageItemCategory = [
+  { icon: <ArmouryHead />, label: '头部防具', id: 'head' },
+  { icon: <ArmouryBody />, label: '身体防具', id: 'body' },
+  { icon: <ArmouryHands />, label: '手部防具', id: 'hands' },
+  { icon: <ArmouryLegs />, label: '腿部防具', id: 'legs' },
+  { icon: <ArmouryFeet />, label: '脚部防具', id: 'feet' },
+  { icon: <ArmouryEarrings />, label: '耳环', id: 'earrings' },
+  { icon: <ArmouryNecklace />, label: '项链', id: 'necklace' },
+  { icon: <ArmouryBracelets />, label: '手镯', id: 'bracelets' },
+  { icon: <ArmouryRing />, label: '戒指', id: 'ring' },
+  { icon: <ItemCategoryDye />, label: '染剂', id: 'dye' },
+  { icon: <ItemCategorySoulCrystal />, label: '时尚配饰', id: 'fashion' },
+]
 
 export default function Page({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined } }) {
   return (
@@ -16,11 +45,22 @@ export default function Page({ searchParams }: { searchParams?: { [key: string]:
           color: c.color,
           _key: c.key,
         }))}
-      ></HomePageTilesRow>
+      />
       <HomePageH1>武器</HomePageH1>
-      <HomePageMainArm></HomePageMainArm>
-      <HomePageH1>部位</HomePageH1>
-      <HomePageH1>专题</HomePageH1>
+      <HomePageMainArm />
+      <HomePageH1>类型</HomePageH1>
+      <div className="flex flex-wrap justify-center">
+        {homePageItemCategory.map((cate) => (
+          <Link href="/" className="group relative" key={cate.id}>
+            <div className="w-16 h-16 p-4 mt-2 mb-1 text-gray-500 transition-all origin-bottom group-hover:scale-150 group-hover:text-primary-500">
+              {cate.icon}
+            </div>
+            <div className="text-center absolute bottom-0 w-full transition-all scale-50 opacity-0 group-hover:scale-100 group-hover:opacity-100">
+              {cate.label}
+            </div>
+          </Link>
+        ))}
+      </div>
     </main>
   )
 }
--- components/aa-root-layout.tsx
+++ components/aa-root-layout.tsx
@@ -1,21 +1,15 @@
-import { Link } from '@nextui-org/link'
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/navbar'
+import { Navbar, NavbarBrand, NavbarContent } from '@nextui-org/navbar'
 import React from 'react'
-import { FaUmbrella } from 'react-icons/fa'
 import {
   AppHomeMenuInventory,
-  AppPaymentSymbol,
-  ArmouryBody,
-  ArmouryMainArm,
-  ArmouryRing,
   FellowActivity18,
-  ItemCategoryCrp,
   ItemCategoryDye,
+  ItemCategorySoulCrystal,
   Market,
 } from '../app/_components/_app-icons'
+import { armouryCategory } from '../app/_constants/armouryCategory'
 import { AALogo } from './aa-logo'
 import { AaNavbarItemTextLink } from './aa-navbar-item-text-link'
-import { armouryCategory } from '../app/_constants/armouryCategory'
 
 export function AaRootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -27,7 +21,7 @@ export function AaRootLayout({ children }: { children: React.ReactNode }) {
         <NavbarContent className="hidden sm:flex flex-1" justify="center">
           <AaNavbarItemTextLink title="系列" icon={<FellowActivity18 />} href="/" menu={armouryCategory} />
           <AaNavbarItemTextLink title="单品" icon={<AppHomeMenuInventory />} href="/" menu={armouryCategory} />
-          <AaNavbarItemTextLink title="时尚配饰" icon={<FaUmbrella />} href="/" />
+          <AaNavbarItemTextLink title="时尚配饰" icon={<ItemCategorySoulCrystal />} href="/" />
           <AaNavbarItemTextLink title="染剂" icon={<ItemCategoryDye />} href="/" />
           <AaNavbarItemTextLink title="专题" icon={<Market />} href="/" />
         </NavbarContent>
